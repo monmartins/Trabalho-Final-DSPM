748639e764bf6a78bda6b0518b93d73c7313de6f
AlyssonMacedo <alysson.macedo@gmail.com>
2016-07-07 13:03:21 -0300

    modificação na tela Configuração

--- BalancoBrasil/app/src/main/java/br/ufc/dc/dspm/balancobrasil/Fragments/Settings.java
+++ BalancoBrasil/app/src/main/java/br/ufc/dc/dspm/balancobrasil/Fragments/Settings.java
@@ -2,15 +2,20 @@ package br.ufc.dc.dspm.balancobrasil.Fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import br.ufc.dc.dspm.balancobrasil.R;
 
-public class Settings extends Fragment {
+public class Settings extends Fragment implements View.OnClickListener {
 
+
+    private CheckBox switchCompact;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,7 +23,6 @@ public class Settings extends Fragment {
         super.onCreate(savedInstanceState);
 
         View view;
-
         view = inflater.inflate(R.layout.fragment_settings, container, false);
 
         return view;
@@ -31,4 +35,20 @@ public class Settings extends Fragment {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.frameLayout:
+                if (switchCompact.isChecked())
+                    switchCompact.setChecked(false);
+                else
+                    switchCompact.setChecked(true);
+                break;
+            case R.id.relativeLayoutChooseTheme:
+                Toast.makeText(null,"Alfa",Toast.LENGTH_LONG);
+                break;
+        }
+    }
+
 }
